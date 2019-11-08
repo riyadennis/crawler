81ecf497e08465762995f33c48ef4a5b6212f82b
riyadennis <riyacv@gmail.com>
2019-11-08 17:32:33 +0000

    addded parsing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/riyadennis/crawler
 
 go 1.13
+
+require golang.org/x/net v0.0.0-20191108063844-7e6e90b9ea88
--- internal/sitemap_test.go
+++ internal/sitemap_test.go
@@ -1,7 +1,10 @@
 package internal
 
 import (
-	"errors"
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"strings"
 	"testing"
 )
 
@@ -11,16 +14,21 @@ func TestSiteMap(t *testing.T) {
 		url         string
 		expectedErr error
 	}{
-		{
-			name: "invalid host name",
-			url:  "invalid",
-			expectedErr: errors.New("invalid host name invalid"),
-		},
+		//{
+		//	name: "invalid host name",
+		//	url:  "invalid",
+		//	expectedErr: errors.New("invalid host name invalid"),
+		//},
 		{
 			name: "valid host",
 			url:  "google.co.uk",
 			expectedErr: nil,
 		},
+		//{
+		//	name: "valid host",
+		//	url:  "mail.google.com/mail/u/0/#inbox",
+		//	expectedErr: nil,
+		//},
 	}
 	for _, sc := range scenarios{
 		t.Run(sc.name, func(t *testing.T){
@@ -44,4 +52,27 @@ func checkErr(t *testing.T, actualErr, expectedErr error) {
 				actualErr.Error(), expectedErr.Error())
 		}
 	}
+}
+
+func testHTMLParsing(){
+	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
 }
