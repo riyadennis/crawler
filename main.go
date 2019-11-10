1e7c3a87bea499b99563d7fa9ad33460c4fa8325
Riya Dennis <riyacv@gmail.com>
2019-11-10 21:21:06 +0000

    new lines for readability

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ func main() {
 	rootURL := flag.String("root", "https://monzo.com", "root ur2l")
 	depth := flag.Int("depth", 2, "depth for crawling")
 	flag.Parse()
+
 	webCrawler, err := internal.NewWebCrawler(*rootURL)
 	if err != nil {
 		panic(err)
 	}
 	ch := make(chan map[int]map[int]string, *depth)
 	go webCrawler.Crawl(*rootURL, *depth, ch)
+
 	internal.Display(ch)
 	close(ch)
 }
